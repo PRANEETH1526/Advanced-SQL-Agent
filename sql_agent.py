9e8a5dcc61b113fdd6db4e071007fc5922b389ac
Lorenzo Policar <policarlorenzo@gmail.com>
2025-02-02 15:53:00 +1000

    Add Azure OpenAI LLMs and connect to database toolkit

--- sql_agent.py
+++ sql_agent.py
@@ -1,1 +1,40 @@
+import operator
+import os
+import re
+from typing import Annotated, Any, TypedDict
 
+from dotenv import load_dotenv
+from langchain_community.agent_toolkits import SQLDatabaseToolkit
+from langchain_community.utilities import SQLDatabase
+from langchain_core.messages import AIMessage, HumanMessage, RemoveMessage, ToolMessage
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnableLambda, RunnableWithFallbacks
+from langchain_core.tools import tool
+from langchain_openai import AzureChatOpenAI
+from langgraph.constants import Send
+from langgraph.graph import END, START, StateGraph
+from langgraph.graph.message import AnyMessage, add_messages
+from langgraph.prebuilt import ToolNode
+from pydantic import BaseModel, Field
+
+load_dotenv()
+
+DATABASE_URI = "" 
+
+
+def download_db():
+    db = SQLDatabase.from_uri(DATABASE_URI)
+    return db
+
+
+db = download_db()
+mini_llm = AzureChatOpenAI(
+    model_name="gpt-4o-mini",
+    api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
+    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT_4O_MINI"),
+    api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+    temperature=0,
+)
+
+llm = AzureChatOpenAI(
+    model_name="gpt-4o",
