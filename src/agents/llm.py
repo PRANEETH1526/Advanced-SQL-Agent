18933cde9957bc0b4ae8f1d9136ebb4f577a6766
Lorenzo Policar <policarlorenzo@gmail.com>
2025-03-27 11:52:57 +1000

    Restructured project

---
+++ src/agents/llm.py
@@ -0,0 +1,21 @@
+from langchain_openai import AzureChatOpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+mini_llm = AzureChatOpenAI(
+    model_name="gpt-4o-mini",
+    api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
+    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT_4O_MINI"),
+    api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+    temperature=0,
+)
+
+llm = AzureChatOpenAI(
+    model_name="gpt-4o",
+    api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
+    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT_4O"),
+    api_key=os.getenv("AZURE_OPENAI_API_KEY_4O"),
+    temperature=0,
+)
---
+++ src/agents/sql_agent/structured_outputs.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, Field
+
+class Subtasks(BaseModel):
+    subtasks: list[str] = Field(
+        description="The list of subtasks to be transformed into queries"
+    )
+
+class Query(BaseModel):
+    query: str = Field(description="The SQL query to be executed")
+    reasoning: str = Field(description="The reasoning for the query")
+
+class SufficientTables(BaseModel):
+    sufficient: bool = Field(
+        description="Whether the information from the tables are sufficient to answer the user question"
+    )
+    reason: str = Field(description="The reason for the answer")
+
+class TransformUserQuestion(BaseModel):
+    question: str = Field(description="The transformed user question")
